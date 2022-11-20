f48d6ddd7bae8ce7428029c1b800b95219ae5735
sangheonLee777 <shl951012@gmail.com>
2022-11-21 04:56:26 +0900

    good

---
+++ js/quotes.js
@@ -0,0 +1,61 @@
+const quotes = [
+  {
+    quote: "asdf",
+    author: "이상헌",
+  },
+  {
+    quote: "qwer ",
+    author: "이상헌",
+  },
+  {
+    quote: "zxcv ",
+    author: "이상헌",
+  },
+  {
+    quote: "123",
+    author: "이상헌",
+  },
+  {
+    quote: "456",
+    author: "이상헌",
+  },
+  {
+    quote: "6789",
+    author: "이상헌",
+  },
+  {
+    quote: "0000",
+    author: "이상헌",
+  },
+  {
+    quote: "ㅁㅁㅁㅁ",
+    author: "이상헌",
+  },
+  {
+    quote: "ㅇㅇㅇㅇ",
+    author: "이상헌",
+  },
+  {
+    quote: "ㅏㅏㅏㅏ",
+    author: "이상헌",
+  },
+
+
+
+
+
+
+
+
+
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * 10)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
+
+
+
